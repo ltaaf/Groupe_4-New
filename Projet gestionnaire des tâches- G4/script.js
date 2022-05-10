b9a3abf1647fb95e75908d76df298f24305006b6
AFO ABDEL FADIL <45314779+ltaaf@users.noreply.github.com>
2022-05-10 07:58:53 +0000

    Delete script.js

--- Projet gestionnaire des tâches- G4/script.js
+++
@@ -1,9 +0,0 @@
-function affiche() {
-  var x = document.forms["inputs"];
-  var result = "";
-  
-  for (var i = 0; i < x.length - 1; i++) {
-      result += "<p>" + x.elements[i].value + "</p>";
-      document.getElementById("Output").innerHTML = result;
-  }
-}
